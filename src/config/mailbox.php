52dd05d35f4a84ec8fdf8c8d6aef3ef468521bbe
trongloikt192 <trongloikt192@gmail.com>
2021-04-06 14:38:39 +0700

    imp#. update mailbox constant

--- src/config/mailbox.php
+++ src/config/mailbox.php
@@ -1,13 +1,13 @@
 <?php
 
 // Account
-define('MAIL_ACTIVE_ACCOUNT', 'mail-active-account');
-define('MAIL_REMAIN_ACCOUNT', 'mail-remain-account');
+define('MAIL_ACTIVE_ACCOUNT'                , 'usr001');
+define('MAIL_REMAIN_ACCOUNT'                , 'usr002');
 
 // Status report daily
-define('MAIL_REPORT_DAILY_TOP_USR_GETLINK', 'mail-remain-account');
-define('MAIL_REPORT_DAILY_BW_SERVER_USAGE', 'mail-remain-account');
+define('MAIL_REPORT_DAILY_TOP_USR_GETLINK'  , 'rpt001');
+define('MAIL_REPORT_DAILY_BW_SERVER_USAGE'  , 'rpt002');
 
 // Notify
-define('MAIL_NOTIFY_HOST_ACCOUNT_ERROR', 'mail-remain-account');
-define('MAIL_NOTIFY_USR_UPGRADE_VIP', 'mail-remain-account');
+define('MAIL_NOTIFY_HOST_ACCOUNT_ERROR'     , 'noti001');
+define('MAIL_NOTIFY_USR_UPGRADE_VIP'        , 'noti002');
