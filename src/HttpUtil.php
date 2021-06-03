cdeeef3a00263d9e3003229c0fc4c8c570e4012d
trongloikt192 <trongloikt192@gmail.com>
2021-06-03 16:17:10 +0700

    fix#rcopCB4i. get_header failed

--- src/HttpUtil.php
+++ src/HttpUtil.php
@@ -397,7 +397,8 @@ class HttpUtil
                 'verify_peer_name' => false,
             ],
             'http' => [
-                'timeout' => 3
+                'timeout' => 3,
+                'header'  => "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2) Gecko/20070219 Firefox/2.0.0.2'\r\n"
             ]
         ]);
         $file_headers = get_headers($url, true, $context);
