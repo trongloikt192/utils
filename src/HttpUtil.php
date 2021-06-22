858294a0783af35cea1213dcfe42764a6f0bfb29
trongloikt192 <trongloikt192@gmail.com>
2021-06-22 16:36:33 +0700

    imp#Release. version 4.4.2

--- src/HttpUtil.php
+++ src/HttpUtil.php
@@ -398,7 +398,7 @@ class HttpUtil
             ],
             'http' => [
                 'method'  => 'HEAD',
-                'timeout' => 15,
+                'timeout' => 60,
                 'header'  => "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2) Gecko/20070219 Firefox/2.0.0.2'\r\n"
             ]
         ]);
