fbacacef28e1bf0163fde7fe10b806171b3d900d
trongloikt192 <trongloikt192@gmail.com>
2021-08-23 15:52:56 +0700

    fix#. grabImage

--- src/ImageUtil.php
+++ src/ImageUtil.php
@@ -19,7 +19,13 @@ class ImageUtil
     {
         $resource = fopen($saveTo, 'wb');
         $client   = new Client();
-        $client->request('GET', $url, ['sink' => $resource]);
+        $client->request('GET', $url, [
+            'sink' => $resource,
+            'headers' => [
+                'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
+                'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+            ]
+        ]);
     }
 
     /**
