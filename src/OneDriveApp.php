b2a366d3505ad2e5d818234cd72133e0456bdef9
trongloikt192 <trongloikt192@gmail.com>
2021-10-28 14:30:26 +0700

    imp#. OneDriveApp.php

---
+++ src/OneDriveApp.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace trongloikt192\Utils;
+
+use Microsoft\Graph\Graph;
+use Microsoft\Graph\Model\UploadSession;
+
+class OneDriveApp
+{
+    private $_Graph;
+
+    /**
+     * OneDriveApp constructor.
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $guzzle       = new \GuzzleHttp\Client();
+        $tenantId     = $config['tenantId'];
+        $clientId     = $config['clientId'];
+        $clientSecret = $config['clientSecret'];
+        $username     = $config['username'];
+        $password     = $config['password'];
+
+        $url          = 'https://login.microsoftonline.com/' . $tenantId . '/oauth2/token';
+        $user_token   = json_decode($guzzle->post($url, [
+            'form_params' => [
+                'client_id'     => $clientId,
+                'client_secret' => $clientSecret,
+                'resource'      => 'https://graph.microsoft.com/',
+                'grant_type'    => 'password',
+                'username'      => $username,
+                'password'      => $password
+            ],
+        ])->getBody()->getContents(), true);
+
+        $this->_Graph = new Graph();
+        $this->_Graph->setAccessToken($user_token->access_token);
+    }
+
+    /**
+     * @param $sourcePath
+     * @param $destFolder
+     * @param null $filename
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Microsoft\Graph\Exception\GraphException
+     */
+    public function uploadFile($sourcePath, $destFolder, $filename = null)
+    {
+        $filename = $filename ?? basename($sourcePath);
+
+        $maxUploadSize = 1024 * 1024 * 4;
+        if (filesize($sourcePath) < $maxUploadSize) {
+            $this->_Graph->createRequest('PUT', '/me/drive/root:/' . trim($destFolder, '/') . '/' . $filename . ':/content')->upload($sourcePath);
+        } else {
+            $uploadSession = $this->_Graph->createRequest('POST', '/me/drive/root:/' . trim($destFolder, '/') . '/' . $filename . ':/createUploadSession')
+                ->addHeaders(['Content-Type' => 'application/json'])
+                ->attachBody([
+                    'item' => [
+                        '@microsoft.graph.conflictBehavior' => 'replace'
+                    ]
+                ])
+                ->setReturnType(UploadSession::class)
+                ->execute();
+
+            $handle        = fopen($sourcePath, 'rb');
+            $fileSize      = fileSize($sourcePath);
+            $chunkSize     = 1024 * 1024 * 64;
+            $prevBytesRead = 0;
+            while (!feof($handle)) {
+                $bytes     = fread($handle, $chunkSize);
+                $bytesRead = ftell($handle);
+
+                $this->_Graph->createRequest('PUT', $uploadSession->getUploadUrl())
+                    ->addHeaders([
+                        'Connection'     => 'keep-alive',
+                        'Content-Length' => ($bytesRead - $prevBytesRead),
+                        'Content-Range'  => 'bytes ' . $prevBytesRead . '-' . ($bytesRead - 1) . '/' . $fileSize,
+                    ])
+                    ->setReturnType(UploadSession::class)
+                    ->attachBody($bytes)
+                    ->execute();
+
+                $prevBytesRead = $bytesRead;
+            }
+        }
+    }
+}
