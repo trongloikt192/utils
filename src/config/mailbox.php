121882ee702a2d684f9eb5052b52b142c52a117d
trongloikt192 <trongloikt192@gmail.com>
2022-12-21 09:48:40 +0700

    imp#. rename re-upload mail constant

--- src/config/mailbox.php
+++ src/config/mailbox.php
@@ -15,10 +15,10 @@ const MAIL_NOTIFY_TRANSACTION_INVALID = 'noti003';
 const MAIL_NOTIFY_DELETE_POST         = 'noti004';
 
 // Re-upload
-const MAIL_REUP_USER_REQUEST                = 'reup001a';
+const MAIL_REUP_USER_REQUEST_TO_UPLOADER    = 'reup001a';
 const MAIL_REUP_UPLOADER_ACCEPT_TO_USER     = 'reup002a';
 const MAIL_REUP_UPLOADER_ACCEPT_TO_UPLOADER = 'reup002b';
-const MAIL_REUP_UPLOADER_SUBMIT_RESULT      = 'reup003a';
+const MAIL_REUP_UPLOADER_SUBMIT_TO_ADMIN    = 'reup003a';
 const MAIL_REUP_SUCCESS_TO_USER             = 'reup004a';
 const MAIL_REUP_SUCCESS_TO_UPLOADER         = 'reup004b';
 const MAIL_REUP_FAILURE_TO_USER             = 'reup005a';
