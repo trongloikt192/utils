d1f054e76a38d72ad2be0ef7e25b40d5f18c864a
LoiTL <trongloi-le@am-bition.vn>
2023-11-20 19:29:28 +0700

    feat#. get file info
    (cherry picked from commit 33b99394471e06a2d5b8480aa3a767116882201b)

--- src/GetLinkFunction.php
+++ src/GetLinkFunction.php
@@ -10,6 +10,8 @@ namespace trongloikt192\Utils;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
+use trongloikt192\Utils\Exceptions\UtilException;
+use trongloikt192\Utils\Host\RapidgatorApi;
 
 class GetLinkFunction
 {
@@ -28,13 +30,14 @@ class GetLinkFunction
     {
         $longURL      = urlencode($link);
         $providerList = [
-            // "http://ouo.io/api/UqgBB0RM?s={$link}",
+             "http://ouo.io/api/UqgBB0RM?s={$link}",
             // "http://shink.in/stxt/0/id/132106/auth_token/uqBeFx?s={$link}",
             // "http://shink.in/stxt/0/id/140178/auth_token/4ETc03?s={$link}",
             // "http://short.am/s/17768?s={$link}",
             // "https://licklink.net/full/?api=0d2bedbae872e4f3db38bba8917a81828ae4132b&url={$linkEncode}&type=2",
-            // "https://123link.pw/full/?api=fd886e7c09b3d9dc09aafbfa7cda474465b5030c&url={$linkEncode}&type=2",
-            "https://link4m.co/api-shorten/v2?api=63773e752fc559675a34d1c5&url={$longURL}"
+            // "https://link4m.co/api-shorten/v2?api=63773e752fc559675a34d1c5&url={$longURL}",
+            // "https://link1s.com/api?api=d81b6b59b1b20102eb7a37ee79241f146a80e129&url={$longURL}",
+            // "https://megaurl.io/api?api=5b71f70c40494e1a3e884e3606b018eef2d09ac1&url={$longURL}"
         ];
 
         // CHỌN RA QUẢNG CÁO KẾ TIẾP
@@ -48,29 +51,21 @@ class GetLinkFunction
             $nextStackNumber = 0;
         }
         Cache::put($cacheKey, $nextStackNumber, Carbon::tomorrow());
-        return $providerList[$stackNumber];
 
-        /* UPDATE 2019-02-09 : Trả trực tiếp link, bỏ qua bước call api, để tăng tốc độ
-         *
-        // Get cURL resource
-        $curl = curl_init();
-        // Set some options - we are passing in a useragent too here
-        curl_setopt_array($curl, array(
-            CURLOPT_RETURNTRANSFER  => 1,
-            CURLOPT_URL             => $url,
-            CURLOPT_USERAGENT       => 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0'
-        ));
-        // Send the request & save response to $resp
-        $res = curl_exec($curl);
-        // Close request to clear up some resources
-        curl_close($curl);
-
-        $shortLink = json_decode($res, 1);
-        if (isset($shortLink['shortenedUrl']) && !empty($shortLink['shortenedUrl'])) {
-            return $shortLink['shortenedUrl'];
+        // Get ads link
+        $requestURL = $providerList[$stackNumber];
+        if ($stackNumber == 0) {
+            return file_get_contents($requestURL);
         }
 
-        return $res;*/
+        $result = @json_decode(file_get_contents($requestURL),TRUE);
+        if($result["status"] !== 'success') {
+            $adsLink = $link;
+        } else {
+            $adsLink = $result["shortenedUrl"];
+        }
+
+        return $adsLink;
     }
 
     /**
@@ -88,7 +83,7 @@ class GetLinkFunction
     }
 
     /**
-     * Get provider và code từ link đầu vào
+     * Get provider và code (và filename nếu có) từ link đầu vào
      *
      * @param string $link
      * @return array
@@ -98,32 +93,30 @@ class GetLinkFunction
         $result = array('provider' => null, 'code' => null, 'status' => FALSE);
 
         $regex = [
-            HOST_FSHARE_VN          => ["/fshare.vn\/file\/(\w{1,})/"]
-            , HOST_4SHARE_VN        => ["/4share.vn\/f\/(\w{1,})/"]
-            , HOST_TENLUA_VN        => ["/tenlua.vn\/[0-9a-zA-Z]+\/(\w{1,})/"]
-            , HOST_YOUTUBE_COM      => ["/youtube.com\/watch\?v=([0-9a-zA-Z-]+)/", "/youtu.be\/([0-1a-zA-Z-]+)/"]
-            , HOST_TAILIEU_VN       => ["/tailieu.vn\/([doc|bst]{3}\/[a-zA-Z0-9-=.]+\.html)/"]
-            , HOST_ZIPPYSHARE_COM   => ["/zippyshare.com\/v\/([0-9a-zA-Z]+)\/file.html/"]
-            , HOST_ALFAFILE_NET     => ["/alfafile.net\/file\/([0-9a-zA-Z]+)/"]
-            , HOST_EXTMATRIX_COM    => ["/extmatrix.com\/files\/([0-9a-zA-Z]+)/"]
-            , HOST_DATAFILE_COM     => ["/datafile.com\/d\/([0-9a-zA-Z]+)/"]
-            , HOST_DEPFILE_COM      => ["/depfile.com\/([0-9a-zA-Z]+)/"]
-            , HOST_DEPOSITFILES_COM => ["/depositfiles.com\/files\/([0-9a-zA-Z]+)/"]
-            , HOST_FILEFACTORY_COM  => ["/filefactory.com\/file\/([0-9a-zA-Z]+)/"]
-            , HOST_FILENEXT_COM     => ["/filenext.com\/([0-9a-zA-Z]+)/"]
-            , HOST_HITFILE_NET      => ["/hitfile.net\/([0-9a-zA-Z]+)/", "/hil.to\/([0-9a-zA-Z]+)/"]
-            , HOST_KATFILE_COM      => ["/katfile.com\/([0-9a-zA-Z]+)/"]
-            , HOST_KEEP2SHARE_CC    => ["/keep2share.cc\/file\/([0-9a-zA-Z]+)/", "/k2s.cc\/file\/([0-9a-zA-Z]+)/", "/keep2s.cc\/file\/([0-9a-zA-Z]+)/", "/k2share.cc\/file\/([0-9a-zA-Z]+)/"]
-            , HOST_NITROFLARE_COM   => ["/nitroflare.com\/view\/([0-9a-zA-Z]+)/", '/nitro.download\/view\/([0-9a-zA-Z]+)/']
-            , HOST_UPLOADED_NET     => ["/uploaded.net\/file\/([0-9a-zA-Z]+)/", "/uploaded.to\/file\/([0-9a-zA-Z]+)/", "/ul.to\/([0-9a-zA-Z]+)/"]
-            , HOST_UPLOADGIG_COM    => ["/uploadgig.com\/file\/download\/([0-9a-zA-Z]+)/"]
-            , HOST_UPTOBOX_COM      => ["/uptobox.com\/([0-9a-zA-Z]+)/"]
-            , HOST_PREFILES_COM     => ["/prefiles.com\/([0-9a-zA-Z]+)/"]
-            , HOST_RAPIDGATOR_NET   => ["/rapidgator.net\/file\/([0-9a-zA-Z]+)/", "/rg.to\/file\/([0-9a-zA-Z]+)/"]
-            , HOST_TURBOBIT_NET => ['/turbobit.net\/([0-9a-zA-Z]+\.html)/', '/turboget.net\/([0-9a-zA-Z]+\.html)/', '/turb.cc\/([0-9a-zA-Z]+\.html)/']
-            , HOST_NHACCUATUI_COM   => ["/nhaccuatui\.com\/bai\-hat\/.*\.([0-9a-zA-Z]+)\.html/", "/nhaccuatui\.com\/video\/.*\.([0-9a-zA-Z]+)\.html/", "/nhaccuatui\.com\/playlist\/.*\.([0-9a-zA-Z]+)\.html/"]
-            , HOST_1FICHIER_COM     => ["/1fichier.com\/\?([0-9a-zA-Z]+)/"]
-            , HOST_GOOGLE_DRIVE_COM => ["/drive.google.com\/file\/d\/([0-9a-zA-Z-_=.]+)/", "/drive.google.com\/open\?id=([0-9a-zA-Z-_=.]+)/"]
+            HOST_FSHARE_VN          => ['/fshare.vn\/file\/(\w{1,})/']
+            , HOST_4SHARE_VN        => ['/4share.vn\/f\/(\w{1,})/']
+            , HOST_ZIPPYSHARE_COM   => ['/zippyshare.com\/v\/([0-9a-zA-Z]+)\/file.html/']
+            , HOST_ALFAFILE_NET     => ['/alfafile.net\/file\/([0-9a-zA-Z]+)/']
+            , HOST_EXTMATRIX_COM    => ['/extmatrix.com\/files\/([0-9a-zA-Z]+)/']
+            //, HOST_DATAFILE_COM     => ["/datafile.com\/d\/([0-9a-zA-Z]+)/"]
+            //, HOST_DEPFILE_COM      => ["/depfile.com\/([0-9a-zA-Z]+)/"]
+            //, HOST_DEPOSITFILES_COM => ["/depositfiles.com\/files\/([0-9a-zA-Z]+)/"]
+            //, HOST_FILEFACTORY_COM  => ["/filefactory.com\/file\/([0-9a-zA-Z]+)/"]
+            , HOST_FILENEXT_COM     => ['/filenext.com\/([0-9a-zA-Z]+)/']
+            , HOST_HITFILE_NET      => ['/hitfile.net\/([0-9a-zA-Z]+)/', '/hil.to\/([0-9a-zA-Z]+)/']
+            , HOST_KATFILE_COM      => ['/katfile.com\/([0-9a-zA-Z]+)/']
+            , HOST_KEEP2SHARE_CC    => ['/keep2share.cc\/file\/([0-9a-zA-Z]+)/', '/k2s.cc\/file\/([0-9a-zA-Z]+)/', '/keep2s.cc\/file\/([0-9a-zA-Z]+)/', '/k2share.cc\/file\/([0-9a-zA-Z]+)/']
+            , HOST_NITROFLARE_COM   => ['/nitroflare.com\/view\/([0-9a-zA-Z]+)\/?(.+)?/', '/nitro.download\/view\/([0-9a-zA-Z]+)\/?(.+)?/']
+            , HOST_UPLOADED_NET     => ['/uploaded.net\/file\/([0-9a-zA-Z]+)/', '/uploaded.to\/file\/([0-9a-zA-Z]+)/', '/ul.to\/([0-9a-zA-Z]+)/']
+            , HOST_UPLOADGIG_COM    => ['/uploadgig.com\/file\/download\/([0-9a-zA-Z]+)/']
+            , HOST_UPTOBOX_COM      => ['/uptobox.com\/([0-9a-zA-Z]+)/']
+            , HOST_PREFILES_COM     => ['/prefiles.com\/([0-9a-zA-Z]+)/']
+            , HOST_RAPIDGATOR_NET   => ['/rapidgator.net\/file\/([0-9a-zA-Z]+)\/?(.+)?/', '/rg.to\/file\/([0-9a-zA-Z]+)\/?(.+)?/']
+            , HOST_TURBOBIT_NET     => ['/turbobit.net\/([0-9a-zA-Z]+\.html)/', '/turboget.net\/([0-9a-zA-Z]+\.html)/', '/turb.cc\/([0-9a-zA-Z]+\.html)/']
+            , HOST_1FICHIER_COM     => ['/1fichier.com\/\?([0-9a-zA-Z]+)/']
+            , HOST_GOOGLE_DRIVE_COM => ['/drive.google.com\/file\/d\/([0-9a-zA-Z-_=.]+)/', '/drive.google.com\/open\?id=([0-9a-zA-Z-_=.]+)/']
+            , HOST_ONEDRIVE_COM     => ['/sharepoint.com\/.*\/download.aspx\?share=([\w\_\-\+]+)/']
+            , HOST_VNLINKS_NET      => ['/vnlinks.net\/file\/(\w{1,})/']
         ];
 
         foreach ($regex as $provider => $arrRegx) {
@@ -131,6 +124,7 @@ class GetLinkFunction
                 if (preg_match($regx, $link, $match) == 1) {
                     $result['provider'] = $provider;
                     $result['code']     = $match[1];
+                    $result['filename'] = isset($match[2]) ? trim(utf8_decode(urldecode($match[2]))) : null;
                     $result['link']     = self::reformatLink($result['provider'], $result['code']) ?? $link;
                     $result['status']   = TRUE;
                     return $result;
@@ -150,6 +144,8 @@ class GetLinkFunction
     public static function reformatLink($provider, $code)
     {
         switch ($provider) {
+            case HOST_1FICHIER_COM:
+                return "https://1fichier.com/?{$code}";
             case HOST_4SHARE_VN:
                 return "https://4share.vn/f/{$code}";
             case HOST_FSHARE_VN:
@@ -178,6 +174,42 @@ class GetLinkFunction
         }
 
         return null;
+    }
+
+    /**
+     * Get filename from link and remove domain in filename
+     *
+     * @param $link
+     * @return mixed|null
+     */
+    public static function detectFilenameFromLink($link)
+    {
+        $rs = self::detectProviderAndCodeFromLink($link);
+        if ($rs['status'] !== TRUE) {
+            return null;
+        }
+
+        $provider = $rs['provider'];
+        $filename = null;
+        switch ($provider) {
+            case HOST_NITROFLARE_COM:
+                $filename = $rs['filename'];
+                break;
+
+            case HOST_RAPIDGATOR_NET:
+                $filename = rtrim($rs['filename'], '.html');
+                break;
+
+            default:
+                break;
+        }
+
+        // Remove sanet.st in filename by regex with case insensitive
+        $filename = preg_replace('/sanet\.st/i', '', $filename);
+
+        // Other case
+
+        return $filename;
     }
 
     /**
@@ -211,14 +243,14 @@ class GetLinkFunction
 
     /**
      * Return getlink address
-     * http://download01.vnlinks.net:81/api/<path>
+     * http://download01.vnlinks.net:81/<path>
      * @param $server
      * @param null $path request-getlink, request-getlink-directly
      * @return string
      */
-    public static function getDomainGetLinkFromServerName($server, $path=null)
+    public static function getDomainGetLinkFromServerName($server, $path = null)
     {
-        $result = 'http://' . $server . ':81/api';
+        $result = 'http://' . self::removeSchemeURL($server) . ':81';
         if (strlen($path) > 0) {
             $result .= '/' . ltrim($path, '/');
         }
@@ -265,7 +297,7 @@ class GetLinkFunction
      */
     public static function removeSchemeURL($url)
     {
-        $parse = parse_url($url);
+        $parse  = parse_url($url);
         $scheme = $parse['scheme'] ?? null;
 
         // Skip if not exist scheme
@@ -275,5 +307,152 @@ class GetLinkFunction
 
         // Remove scheme
         return ltrim($url, $scheme . '://');
+    }
+
+    /**
+     * Get file info from link
+     *
+     * @param $linkList
+     * @return null
+     * Ex:
+     * [
+     *   'nitroflare.com' => [
+     *     'code' => [
+     *      'url' => 'abc',
+     *      'filename' => 'abc',
+     *      'filesize' => 123,
+     *     ]
+     *     ...
+     *   ],
+     *   'rapidgator.net' => [
+     *     'code' => [
+     *      'url' => 'abc',
+     *      'filename' => 'abc',
+     *      'filesize' => 123,
+     *     ]
+     *     ...
+     *   ]
+     *   ...
+     * ]
+     */
+    public static function getFileInfoFromLinks($linkList)
+    {
+        $result = [];
+        $groupLinks = [];
+
+        // Group list link by provider
+        foreach ($linkList as $link) {
+            $provider = self::detectProviderFromLink($linkList);
+            if (isset($groupLinks[$provider])) {
+                $groupLinks[$provider][] = $link;
+            } else {
+                $groupLinks[$provider] = [$link];
+            }
+        }
+
+        // Get file info from each provider
+        foreach ($groupLinks as $provider => $links) {
+            switch ($provider) {
+                case HOST_NITROFLARE_COM:
+                    // separate links to 25 links per request
+                    $links = array_chunk($links, 25);
+                    foreach ($links as $link) {
+                        $result[$provider] = array_merge($result[$provider] ?? [], self::getNitroflareFileInfo($link));
+                    }
+                    break;
+
+                case HOST_RAPIDGATOR_NET:
+                    // separate links to 25 links per request
+                    $links = array_chunk($links, 25);
+                    foreach ($links as $link) {
+                        $result[$provider] = array_merge($result[$provider] ?? [], self::getRapidgatorFileInfo($link));
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get nitroflare file info
+     *
+     * @param $linkList
+     * @return array
+     * Ex: [
+     *    'code' => [
+     *       'url'      => 'abc',
+     *       'filename' => 'abc',
+     *       'filesize' => 123,
+     *    ]
+     * ]
+     */
+    public static function getNitroflareFileInfo($linkList) {
+        $result = [];
+
+        $codeList = [];
+        foreach ($linkList as $link) {
+            $codeList[] = self::detectCodeFromLink($link);
+        }
+
+        $data = HttpUtil::g_curlGet('https://nitroflare.com/api/v2/getFileInfo?files=' . implode(',', $codeList));
+        $info = json_decode($data, true );
+        if ($info['type'] == 'success') {
+            $files = $info['result']['files'];
+            foreach ($files as $cd => $file) {
+                $result[$cd] = [
+                    'url'      => $file['url'],
+                    'filename' => $file['name'],
+                    'filesize' => $file['size'],
+                ];
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get rapidgator file info
+     *
+     * @param $linkList
+     * @return array
+     * Ex: [
+     *    'code' => [
+     *       'url'      => 'abc',
+     *       'filename' => 'abc',
+     *       'filesize' => 123,
+     *    ]
+     * ]
+     */
+    public static function getRapidgatorFileInfo($linkList)
+    {
+        $result = [];
+
+        try {
+            // CONNECT API
+            $client = new RapidgatorApi(env('RAPIDGATOR_EMAIL'), env('RAPIDGATOR_PASSWORD'), storage_path('rapidgator.token'));
+
+            // GET FILE SIZE & NAME
+            $response = $client->checkLink(implode(',', $linkList));
+            foreach ($response as $file) {
+                if ($file['status'] != 'ACCESS') {
+                    continue;
+                }
+
+                $code = self::detectCodeFromLink($file['url']);
+                $result[$code] = [
+                    'url'      => $file['url'],
+                    'filename' => $file['filename'],
+                    'filesize' => $file['size'],
+                ];
+            }
+        } catch (\Exception $e) {
+            //
+        }
+
+        return $result;
     }
 }
