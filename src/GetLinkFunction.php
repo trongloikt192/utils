47e0ecbae466324c25e651b00161c5754c715e72
LoiTL <trongloi-le@am-bition.vn>
2023-11-20 19:29:28 +0700

    feat#. get file info
    (cherry picked from commit 33b99394471e06a2d5b8480aa3a767116882201b)

--- src/GetLinkFunction.php
+++ src/GetLinkFunction.php
@@ -10,6 +10,8 @@ namespace trongloikt192\Utils;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
+use trongloikt192\Utils\Exceptions\UtilException;
+use trongloikt192\Utils\Host\RapidgatorApi;
 
 class GetLinkFunction
 {
@@ -81,7 +83,7 @@ class GetLinkFunction
     }
 
     /**
-     * Get provider và code từ link đầu vào
+     * Get provider và code (và filename nếu có) từ link đầu vào
      *
      * @param string $link
      * @return array
@@ -104,12 +106,12 @@ class GetLinkFunction
             , HOST_HITFILE_NET      => ['/hitfile.net\/([0-9a-zA-Z]+)/', '/hil.to\/([0-9a-zA-Z]+)/']
             , HOST_KATFILE_COM      => ['/katfile.com\/([0-9a-zA-Z]+)/']
             , HOST_KEEP2SHARE_CC    => ['/keep2share.cc\/file\/([0-9a-zA-Z]+)/', '/k2s.cc\/file\/([0-9a-zA-Z]+)/', '/keep2s.cc\/file\/([0-9a-zA-Z]+)/', '/k2share.cc\/file\/([0-9a-zA-Z]+)/']
-            , HOST_NITROFLARE_COM   => ['/nitroflare.com\/view\/([0-9a-zA-Z]+)/', '/nitro.download\/view\/([0-9a-zA-Z]+)/']
+            , HOST_NITROFLARE_COM   => ['/nitroflare.com\/view\/([0-9a-zA-Z]+)\/?(.+)?/', '/nitro.download\/view\/([0-9a-zA-Z]+)\/?(.+)?/']
             , HOST_UPLOADED_NET     => ['/uploaded.net\/file\/([0-9a-zA-Z]+)/', '/uploaded.to\/file\/([0-9a-zA-Z]+)/', '/ul.to\/([0-9a-zA-Z]+)/']
             , HOST_UPLOADGIG_COM    => ['/uploadgig.com\/file\/download\/([0-9a-zA-Z]+)/']
             , HOST_UPTOBOX_COM      => ['/uptobox.com\/([0-9a-zA-Z]+)/']
             , HOST_PREFILES_COM     => ['/prefiles.com\/([0-9a-zA-Z]+)/']
-            , HOST_RAPIDGATOR_NET   => ['/rapidgator.net\/file\/([0-9a-zA-Z]+)/', '/rg.to\/file\/([0-9a-zA-Z]+)/']
+            , HOST_RAPIDGATOR_NET   => ['/rapidgator.net\/file\/([0-9a-zA-Z]+)\/?(.+)?/', '/rg.to\/file\/([0-9a-zA-Z]+)\/?(.+)?/']
             , HOST_TURBOBIT_NET     => ['/turbobit.net\/([0-9a-zA-Z]+\.html)/', '/turboget.net\/([0-9a-zA-Z]+\.html)/', '/turb.cc\/([0-9a-zA-Z]+\.html)/']
             , HOST_1FICHIER_COM     => ['/1fichier.com\/\?([0-9a-zA-Z]+)/']
             , HOST_GOOGLE_DRIVE_COM => ['/drive.google.com\/file\/d\/([0-9a-zA-Z-_=.]+)/', '/drive.google.com\/open\?id=([0-9a-zA-Z-_=.]+)/']
@@ -122,6 +124,7 @@ class GetLinkFunction
                 if (preg_match($regx, $link, $match) == 1) {
                     $result['provider'] = $provider;
                     $result['code']     = $match[1];
+                    $result['filename'] = isset($match[2]) ? trim(utf8_decode(urldecode($match[2]))) : null;
                     $result['link']     = self::reformatLink($result['provider'], $result['code']) ?? $link;
                     $result['status']   = TRUE;
                     return $result;
@@ -171,6 +174,42 @@ class GetLinkFunction
         }
 
         return null;
+    }
+
+    /**
+     * Get filename from link and remove domain in filename
+     *
+     * @param $link
+     * @return mixed|null
+     */
+    public static function detectFilenameFromLink($link)
+    {
+        $rs = self::detectProviderAndCodeFromLink($link);
+        if ($rs['status'] !== TRUE) {
+            return null;
+        }
+
+        $provider = $rs['provider'];
+        $filename = null;
+        switch ($provider) {
+            case HOST_NITROFLARE_COM:
+                $filename = $rs['filename'];
+                break;
+
+            case HOST_RAPIDGATOR_NET:
+                $filename = rtrim($rs['filename'], '.html');
+                break;
+
+            default:
+                break;
+        }
+
+        // Remove sanet.st in filename by regex with case insensitive
+        $filename = preg_replace('/sanet\.st/i', '', $filename);
+
+        // Other case
+
+        return $filename;
     }
 
     /**
@@ -268,5 +307,152 @@ class GetLinkFunction
 
         // Remove scheme
         return ltrim($url, $scheme . '://');
+    }
+
+    /**
+     * Get file info from link
+     *
+     * @param $linkList
+     * @return null
+     * Ex:
+     * [
+     *   'nitroflare.com' => [
+     *     'code' => [
+     *      'url' => 'abc',
+     *      'filename' => 'abc',
+     *      'filesize' => 123,
+     *     ]
+     *     ...
+     *   ],
+     *   'rapidgator.net' => [
+     *     'code' => [
+     *      'url' => 'abc',
+     *      'filename' => 'abc',
+     *      'filesize' => 123,
+     *     ]
+     *     ...
+     *   ]
+     *   ...
+     * ]
+     */
+    public static function getFileInfoFromLinks($linkList)
+    {
+        $result = [];
+        $groupLinks = [];
+
+        // Group list link by provider
+        foreach ($linkList as $link) {
+            $provider = self::detectProviderFromLink($linkList);
+            if (isset($groupLinks[$provider])) {
+                $groupLinks[$provider][] = $link;
+            } else {
+                $groupLinks[$provider] = [$link];
+            }
+        }
+
+        // Get file info from each provider
+        foreach ($groupLinks as $provider => $links) {
+            switch ($provider) {
+                case HOST_NITROFLARE_COM:
+                    // separate links to 25 links per request
+                    $links = array_chunk($links, 25);
+                    foreach ($links as $link) {
+                        $result[$provider] = array_merge($result[$provider] ?? [], self::getNitroflareFileInfo($link));
+                    }
+                    break;
+
+                case HOST_RAPIDGATOR_NET:
+                    // separate links to 25 links per request
+                    $links = array_chunk($links, 25);
+                    foreach ($links as $link) {
+                        $result[$provider] = array_merge($result[$provider] ?? [], self::getRapidgatorFileInfo($link));
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get nitroflare file info
+     *
+     * @param $linkList
+     * @return array
+     * Ex: [
+     *    'code' => [
+     *       'url'      => 'abc',
+     *       'filename' => 'abc',
+     *       'filesize' => 123,
+     *    ]
+     * ]
+     */
+    public static function getNitroflareFileInfo($linkList) {
+        $result = [];
+
+        $codeList = [];
+        foreach ($linkList as $link) {
+            $codeList[] = self::detectCodeFromLink($link);
+        }
+
+        $data = HttpUtil::g_curlGet('https://nitroflare.com/api/v2/getFileInfo?files=' . implode(',', $codeList));
+        $info = json_decode($data, true );
+        if ($info['type'] == 'success') {
+            $files = $info['result']['files'];
+            foreach ($files as $cd => $file) {
+                $result[$cd] = [
+                    'url'      => $file['url'],
+                    'filename' => $file['name'],
+                    'filesize' => $file['size'],
+                ];
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get rapidgator file info
+     *
+     * @param $linkList
+     * @return array
+     * Ex: [
+     *    'code' => [
+     *       'url'      => 'abc',
+     *       'filename' => 'abc',
+     *       'filesize' => 123,
+     *    ]
+     * ]
+     */
+    public static function getRapidgatorFileInfo($linkList)
+    {
+        $result = [];
+
+        try {
+            // CONNECT API
+            $client = new RapidgatorApi(env('RAPIDGATOR_EMAIL'), env('RAPIDGATOR_PASSWORD'), storage_path('rapidgator.token'));
+
+            // GET FILE SIZE & NAME
+            $response = $client->checkLink(implode(',', $linkList));
+            foreach ($response as $file) {
+                if ($file['status'] != 'ACCESS') {
+                    continue;
+                }
+
+                $code = self::detectCodeFromLink($file['url']);
+                $result[$code] = [
+                    'url'      => $file['url'],
+                    'filename' => $file['filename'],
+                    'filesize' => $file['size'],
+                ];
+            }
+        } catch (\Exception $e) {
+            //
+        }
+
+        return $result;
     }
 }
